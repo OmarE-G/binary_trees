f3187694dc43cfba89461a46196de4db4bc2d25c
Omar Essam <omar161157@eng.psu.edu.eg>
2024-03-30 01:57:24 +0200

    New commit

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+#define max(a, b) (a > b ? a : b)
+
+
+/**
+ * binary_tree_height - height
+ * @tree: root node of the tree
+ * Return: Height of the tree
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (max(
+		(tree->left ? 1 + binary_tree_height(tree->left) : 0),
+		(tree->right ? 1 + binary_tree_height(tree->right) : 0)
+		));
+}
