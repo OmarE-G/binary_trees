e34737b569027298c6e1a22414c54a1512169e49
Omar Essam <omar161157@eng.psu.edu.eg>
2024-03-30 00:18:22 +0200

    New commit

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - bt node
+ * @value: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * Return: pointer
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (parent == NULL)
+		free(node), node = NULL;
+
+	if (node)
+	{
+		node->left = NULL, node->right = NULL;
+		node->n = value;
+
+		if (parent->left == NULL)
+			node->parent = parent, parent->left = node;
+		else
+			parent->left->parent = node,
+			parent->left = node, node->parent = parent;
+	}
+
+	return (node);
+}
