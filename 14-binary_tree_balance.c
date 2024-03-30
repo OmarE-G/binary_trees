f823b9d1eedbe5780178cdb781e8a801f45aaef8
Omar Essam <omar161157@eng.psu.edu.eg>
2024-03-30 02:29:34 +0200

    New commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,6 +4,25 @@
 #define max(a, b) (a > b ? a : b)
 
 
+
+
+/**
+ * binary_tree_height - height
+ * @tree: root node of the tree
+ * Return: Height of the tree
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (max(
+		(tree->left ? 1 + binary_tree_height(tree->left) : 0),
+		(tree->right ? 1 + binary_tree_height(tree->right) : 0)
+		));
+}
+
 /**
  * binary_tree_balance - balance
  * @tree: root node of the tree
@@ -15,8 +34,8 @@ size_t binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->left) -
-	binary_tree_height(tree->right));
+	return ((tree->left ? binary_tree_height(tree->left) + 1 : 0) -
+	(tree->right ? binary_tree_height(tree->right) + 1 : 0));
 
 }
 
